e51dca1d11cf2a0ee94584571bd3a7a47925886e
debyprast <debyprast18@gmail.com>
2020-04-30 14:16:15 +0700

    FirebaseRealtime

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAmo8hfv8DCpx6DBsb82ouNV5ZOsHrehas",
+  authDomain: "reactjs-db713.firebaseapp.com",
+  databaseURL: "https://reactjs-db713.firebaseio.com",
+  projectId: "reactjs-db713",
+  storageBucket: "reactjs-db713.appspot.com",
+  messagingSenderId: "852413332907",
+  appId: "1:852413332907:web:065e96eadbfc10c34166ee",
+  measurementId: "G-D3WXB77TST"
+};
+
+  export default firebaseConfig;
